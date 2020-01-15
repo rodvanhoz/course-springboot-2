e1b1e68c565bc474bf16ab2218258d54b272d5e7
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@@gmail.com>
2020-01-14 23:51:44 -0200

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -103,6 +103,17 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 
+	public Double getTotal() {
+		
+		double sum = 0;
+		
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
